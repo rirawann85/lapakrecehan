4ca735a5b3542e4dee592ffaab8f8c5b71209372
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-01-20 15:08:05 +0700

    Update random.js

--- random.js
+++ random.js
@@ -1,1 +1,1 @@
-document.getElementById('body').src="//rawcdn.githack.com/rirawann85/script/f3a3ec6d1248c976397500bb02fef21c328e850c/terusmodyar.js";
+document.write('<script src="https://rawcdn.githack.com/rirawann85/script/f3a3ec6d1248c976397500bb02fef21c328e850c/terusmodyar.js' + '"><\/script>');
